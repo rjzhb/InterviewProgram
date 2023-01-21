506786372a64a0fdce613acefd37dbc25f21b2c7
rjzhb <rjzhb222@163.com>
2023-01-21 20:56:36 +0800

    update

---
+++ ISPC/cmake-build-debug/CMakeFiles/ISPC.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ISPC.dir/main.cpp.o"
+  "ISPC"
+  "ISPC.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ISPC.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ WeakReference/cmake-build-debug/CMakeFiles/WeakReference.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/WeakReference.dir/main.cpp.o"
+  "WeakReference"
+  "WeakReference.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/WeakReference.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
